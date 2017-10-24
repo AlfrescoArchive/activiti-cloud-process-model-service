c729b05260689af46c724bf864229f79b5568294
Salaboy <salaboy@gmail.com>
2017-10-24 16:03:42 +0100

    squashing 'feature/cciobotaru-1512-process-model-basic-conf' into 'develop'

--- activiti-cloud-starter-process-model/src/test/java/org/activiti/cloud/process/model/ProcessModelApplicationTest.java
+++ activiti-cloud-starter-process-model/src/test/java/org/activiti/cloud/process/model/ProcessModelApplicationTest.java
@@ -17,10 +17,8 @@
 package org.activiti.cloud.process.model;
 
 import org.activiti.cloud.process.model.ProcessModelApplication;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
