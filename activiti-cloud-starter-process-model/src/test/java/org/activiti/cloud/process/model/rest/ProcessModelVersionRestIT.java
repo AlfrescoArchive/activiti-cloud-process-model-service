35564fd39983959ddd5f99d24473d9821a706098
salaboy <salaboy@gmail.com>
2018-08-31 11:28:11 +0300

    initial clean up

--- activiti-cloud-starter-process-model/src/test/java/org/activiti/cloud/process/model/rest/ProcessModelVersionRestIT.java
+++ activiti-cloud-starter-process-model/src/test/java/org/activiti/cloud/process/model/rest/ProcessModelVersionRestIT.java
@@ -20,6 +20,7 @@ import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.activiti.cloud.process.model.ProcessModelApplication;
 import org.activiti.cloud.services.process.model.core.model.ProcessModel;
 import org.activiti.cloud.services.process.model.jpa.ProcessModelRepository;
+import org.activiti.cloud.services.test.identity.keycloak.interceptor.KeycloakTokenProducer;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
